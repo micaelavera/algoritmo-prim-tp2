6e953c9fba2ed63b090c550b969f8d016a2fe70b
Micaela <micavera2010@hotmail.com>
2017-10-08 20:06:49 -0300

    Cambio de la clase Localidad de un JFrame a un JDialog

---
+++ TrabajoPractico2-P3/src/interfaz/Arista.java
@@ -0,0 +1,39 @@
+package interfaz;
+
+import java.util.ArrayList;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
+
+public class Arista {
+
+	private Coordinate origen;
+	private Coordinate destino;
+	
+	
+	public Arista(Coordinate a,Coordinate b){
+		verificarCoordenadas(a, b);
+		this.origen=a;
+		this.destino=b;
+	}
+
+
+	private void verificarCoordenadas(Coordinate a, Coordinate b) {
+		if(a==null || b==null){
+			 throw new RuntimeException("Null cordinate");
+		}
+	}
+	
+	public void poligono(JMapViewer mapa) {
+        ArrayList<Coordinate> coordenadas2 = new ArrayList<> ();
+        coordenadas2.add(origen);
+        coordenadas2.add(destino);
+        coordenadas2.add(origen);
+
+        MapPolygon polygon = new MapPolygonImpl(coordenadas2);
+        mapa.addMapPolygon(polygon);
+
+    }
+}
