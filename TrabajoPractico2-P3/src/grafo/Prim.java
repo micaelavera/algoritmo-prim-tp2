c1938e0a8c4c2d9d0af2f31bb86cdb16ba0dcb24
Micaela <micavera2010@hotmail.com>
2017-10-13 18:27:37 -0300

    Agregado de un algoritmo de Prim provisorio

--- TrabajoPractico2-P3/src/grafo/Prim.java
+++ TrabajoPractico2-P3/src/grafo/Prim.java
@@ -1,22 +1,32 @@
 package grafo;
 
-public class Prim {
+import java.util.HashSet;
+import java.util.Set;
 
-//	// Algoritmo de Prim
-//	public static GrafoPesado AGM(GrafoPesado grafo,Menu Menu){
-//		GrafoPesado GRAFO = new GrafoPesado(grafo.vertices());
-//		Set<Integer> visitados = new HashSet<> ();
-//		visitados.add(0); // Cualquiera
-//
-//		for(int i=0; i<grafo.vertices()-1; ++i){
-//			Arista a = menorArista(grafo, visitados); // De un amarillo a un negro
-//			GRAFO.agregarArista(a.origen, a.destino, a.peso);
-//			visitados.add(a.destino);
-//			if(i%2==0)
-//				Menu.setProgress("Calculando AGM...",(i*100)/grafo.vertices()-1);
-//		}
-//		
-//		return GRAFO;
-//	}
+public class Prim {
 	
+	// Algoritmo de Prim
+	public static GrafoConPesos AGM(GrafoConPesos grafo){
+		GrafoConPesos _grafo=new GrafoConPesos(grafo.vertices());
+		Set<Integer> verticesVisitados=new HashSet<>();
+		verticesVisitados.add(0);
+		
+		for(int i=0;i<grafo.vertices()-1;++i){
+			Arista a = menorArista(grafo, verticesVisitados); 
+			_grafo.agregarArista(a.getOrigen(), a.getDestino(), a.getPeso());
+			verticesVisitados.add(a.getDestino());
+		}
+		return _grafo;
+	}
+
+	private static Arista menorArista(GrafoConPesos grafo,Set<Integer> verticesVisitados) {
+		Arista ret = new Arista(0, 0, Double.MAX_VALUE);
+		for(Integer i: verticesVisitados)
+			for (Integer j : grafo.vecinos(i))
+				if(!verticesVisitados.contains (j)){
+					if( grafo.consultarPeso(i, j) < ret.getPeso() )
+						ret = new Arista(i, j, grafo.consultarPeso(i, j));
+				}
+			return ret;
+		}
 }
