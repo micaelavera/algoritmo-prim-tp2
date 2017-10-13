c334dd64fdff2f422c5de4d53f76f1350a97c8d6
Micaela <micavera2010@hotmail.com>
2017-10-13 18:38:39 -0300

    Agrego clase Assert para GrafoTest

---
+++ TrabajoPractico2-P3/src/grafo/Assert.java
@@ -0,0 +1,18 @@
+package grafo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+public class Assert {
+
+	//Determina si el set contiene los elementos del arreglo
+	public static void setsIguales(int[] esperado,Set<Integer> obtenido){
+		assertEquals(esperado.length,obtenido.size());
+		for(Integer elemento: esperado)
+			assertTrue(obtenido.contains(elemento));
+		}
+	}
+
+
+
