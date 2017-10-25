fd333cabe092a8ba0a7fa9795f5ff5034fba2f47
Micaela <micavera2010@hotmail.com>
2017-10-25 15:46:07 -0300

    Agrego instancias json

---
+++ TrabajoPractico2-P3/src/tests/MapaTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+import interfaz.Mapa;
+
+import org.junit.Test;
+
+public class MapaTest {
+
+//	private Mapa mapa;
+	
+	@Test
+	public void testMapa() {
+		Mapa mapa=new Mapa();
+		
+	}
+
+}
